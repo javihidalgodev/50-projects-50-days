5c36455112d3ed6202b26b053a2b7a3175aa157c
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2023-10-03 10:11:22 +0200

    Update project 22

--- 22-drawing-app/assets/scripts/code.js
+++ 22-drawing-app/assets/scripts/code.js
@@ -1,11 +1,14 @@
 const canvasContainer = document.querySelector('.canvas-container')
 const canvas = document.querySelector('#board')
-const context = canvas.getContext('2d')
+
+const context = canvas.getContext('2d', {willReadFrequently:true})
 
 let initialX;
 let initialY;
 let lineWidth = 10;
 let lineColor = '#000';
+let boardColor = '#fdfdfd'
+let prevImg;
 
 // Función que dibuja
 const draw = (cursorX, cursorY) => {
@@ -26,6 +29,7 @@ const mouseDown = (e) => {
   initialX = e.offsetX;
   initialY = e.offsetY;
 
+  prevImg = context.getImageData(0, 0, canvas.width, canvas.height)
   draw(initialX, initialY)
   canvas.addEventListener('mousemove', mouseMoving)
 }
@@ -41,12 +45,18 @@ const mouseUp = () => {
 canvas.addEventListener('mousedown', mouseDown)
 canvas.addEventListener('mouseup', mouseUp)
 
+canvas.addEventListener('mouseleave', () => {
+  canvas.removeEventListener('mousemove', mouseMoving)
+})
+
 // Control de los parámetros del dibujo
 const lineWidthDecrease = document.querySelector('#line-width-decrease')
 const lineWidthValue = document.querySelector('#line-width-value')
 const lineWidthIncrease = document.querySelector('#line-width-increase')
 const lineColorValue = document.querySelector('#line-color')
+const boardColorValue = document.querySelector('#board-color')
 const eraser = document.querySelector('#eraser')
+const undo = document.querySelector('#undo')
 
 lineWidthDecrease.addEventListener('click', (e) => {
   lineWidthValue.valueAsNumber > 1 && (
@@ -78,24 +88,41 @@ lineColorValue.addEventListener('change', (e) => {
   mouseDown
 })
 
+boardColorValue.addEventListener('change', (e) => {
+  const prevDraw = context.getImageData(0, 0, canvas.width, canvas.height)
+
+  boardColor = boardColorValue.value
+
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  canvas.style.backgroundColor = boardColor
+  
+  context.putImageData(prevDraw, 0, 0)
+})
+
+undo.addEventListener('click', () => {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  context.putImageData(prevImg, 0, 0)
+})
+
 eraser.addEventListener('click', (e) => {
   const modal = document.createElement('DIV')
   modal.classList.add('modal')
   modal.innerHTML = `
-    <p>¿Estás seguro? Esta acción borrará toda la pizarra:</p>
-    <button class="dialog-button dialog-button--ok">Borrar</button>
-    <button class="dialog-button dialog-button--cancel">Cancelar</button>
+  <p>¿Estás seguro? Esta acción borrará toda la pizarra:</p>
+  <button class="dialog-button dialog-button--ok">Borrar</button>
+  <button class="dialog-button dialog-button--cancel">Cancelar</button>
   `
   canvasContainer.appendChild(modal)
-
+  
   const dialogBtns = Array.from(document.querySelector('.modal').querySelectorAll('button'))
-
+  
   dialogBtns.forEach(btn => {
     btn.addEventListener('click', () => {
       btn.textContent === 'Borrar'
-        ? (
-          context.clearRect(0, 0, canvas.width, canvas.height),
-          canvasContainer.removeChild(modal)
+      ? (
+        context.clearRect(0, 0, canvas.width, canvas.height),
+        canvas.style.backgroundColor = boardColor,
+        canvasContainer.removeChild(modal)
         ) : canvasContainer.removeChild(modal)
     })
   })
