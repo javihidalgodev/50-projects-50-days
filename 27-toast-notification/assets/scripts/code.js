951ce2798a7694266cee9d6e9cbb025c0dfe3f3f
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2023-10-09 10:29:29 +0200

    27-toast-notification first commit

---
+++ 27-toast-notification/assets/scripts/code.js
@@ -0,0 +1,42 @@
+const showBtn = document.querySelector('.show-btn')
+const notificationContainer = document.querySelector('.notification-container')
+
+const messages = [
+  'Mensaje 1',
+  'Mensaje 2',
+  'Mensaje 3',
+  'Mensaje 4'
+]
+
+const typeMsg = [
+  'success',
+  'error',
+  'info'
+]
+
+showBtn.addEventListener('click', () => {
+  getNotification()
+})
+
+const getNotification = () => {
+  const message = getMessage()
+  const type = getType()
+
+  const notification = document.createElement('DIV')
+  notification.classList.add('notification', type)
+  notification.innerHTML = `<span>${message}</span>`
+
+  notificationContainer.append(notification)
+
+  // setTimeout(() => {
+  //   notification.remove()
+  // }, 3000)
+}
+
+const getMessage = () => {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
+
+const getType = () => {
+  return typeMsg[Math.floor(Math.random() * typeMsg.length)]
+}
