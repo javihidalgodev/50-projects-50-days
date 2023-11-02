d06fb950db3bea63e5c260021bbefd48b3d7b98c
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2023-11-02 13:14:30 +0100

    49 first commit

---
+++ 49-todo-list/assets/scripts/code.js
@@ -0,0 +1,66 @@
+const form = document.querySelector('#todo-form')
+const todoInput = document.querySelector('.todo-input')
+const todosContainer = document.querySelector('.todos')
+
+form.addEventListener('submit', grabTodo)
+
+function grabTodo (e) {
+  e.preventDefault()
+  
+  checkTodos()
+  
+  const newTodo = document.createElement('DIV')
+  newTodo.classList = 'todo'
+  newTodo.textContent = todoInput.value
+  
+  newTodo.addEventListener('click', actionControl)
+  newTodo.addEventListener('contextmenu', actionControl)
+  
+  todosContainer.append(newTodo)
+  
+  saveLS()
+  todoInput.value = ''
+}
+
+function actionControl (e) {
+  e.preventDefault()
+  
+  const clicked = e.which
+  const todo = e.target
+  
+  if(clicked === 1) {
+    todo.classList.toggle('complete')
+    saveLS()
+
+  } else if(clicked === 3) {
+    todo.remove()
+    saveLS()
+  }
+  
+  checkTodos()
+}
+
+function checkTodos () {
+  if(todosContainer.childElementCount === 0) {
+    todosContainer.innerHTML = '<div class="no-content">No hay tareas</div>'
+  } else {
+    todosContainer.querySelector('.no-content') && todosContainer.querySelector('.no-content').remove()
+  }
+}
+
+function saveLS () {
+  const todosToSave = todosContainer.innerHTML
+  localStorage.setItem('savedTodos', todosToSave)
+}
+
+const savedTodos = localStorage.getItem('savedTodos')
+todosContainer.innerHTML = savedTodos
+
+if(todosContainer.childElementCount > 0) {
+  todosContainer.childNodes.forEach(todo => {
+    todo.addEventListener('click', actionControl)
+    todo.addEventListener('contextmenu', actionControl)
+  })
+}
+
+checkTodos()
